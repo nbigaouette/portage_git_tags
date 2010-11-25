c933cf0d2b4f7416dcb171263a2f96e1aae879a8
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-11-25 14:56:52 -0500

    Added GPL3 header

--- sync_and_git_tag.sh
+++ sync_and_git_tag.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Copyright 2010 Nicolas Bigaouette <nbigaouette @t gmail com>
+# This file is part of portage_git_tags
+# http://github.com/nbigaouette/portage_git_tags
+#
+# portage_git_tags is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# portage_git_tags is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with git-scripts. If not, see <http://www.gnu.org/licenses/>.
+
 local_portage_dir="portage"
 
 # rsync_mirror="rsync://gentoo.gossamerhost.com/gentoo-distfiles/"
