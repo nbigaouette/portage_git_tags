e97ff345b53b38046fdd09a0a560f6300fe02130
Nicolas Bigaouette <nbigaouette@gmail.com>
2010-11-25 15:08:52 -0500

    Don't put the local portage directory into this repo. Create a new one instead.

--- sync_and_git_tag.sh
+++ sync_and_git_tag.sh
@@ -38,47 +38,67 @@ function die()
 
 function get_latest_portage_snapshot()
 {
-    if [[ ${#@} -ne 3 ]]; then
-        echo "ERROR: get_latest_portage_snapshot() function takes exactly 3 arguments!"
+    if [[ ${#@} -ne 4 ]]; then
+        echo "ERROR: get_latest_portage_snapshot() function takes exactly 4 arguments!"
         echo "Current arguments: ${@}"
         return
     fi
     mirror="${1}"
     portage_snapshot_path="${2}"
     portage_snapshot_file="${3}"
+    local_portage_dir="${4}"
     wget ${mirror}/${portage_snapshot_path}/${portage_snapshot_file}
     wget ${mirror}/${portage_snapshot_path}/${portage_snapshot_file}.gpgsig
     wget ${mirror}/${portage_snapshot_path}/${portage_snapshot_file}.md5sum
     md5sum -c ${portage_snapshot_file}.md5sum || die "md5sum does not match!"
 
     # Remove old directories, extract new and move to wanted location
-    rm -fr ${local_portage_dir} portage
+    [[ "${local_portage_dir}" != "portage" ]] && rm -fr portage
+    rm -fr ${local_portage_dir}/*
     tar xvjf ${portage_snapshot_file}
-    [[ "${local_portage_dir}" != "portage" ]] && mv portage ${local_portage_dir}
+    [[ "${local_portage_dir}" != "portage" ]] && mv portage/* ${local_portage_dir}/
 }
 
 function rsync_portage()
 {
-    if [[ ${#@} -ne 2 ]]; then
+    if [[ ${#@} -ne 3 ]]; then
         echo "ERROR: rsync_portage() function takes exactly 2 arguments!"
         echo "Current arguments: ${@}"
         return
     fi
     PORTAGE_RSYNC_OPTS="${1}"
     rsync_mirror="${2}"
+    local_portage_dir="${3}"
     rsync ${PORTAGE_RSYNC_OPTS} ${rsync_mirror} ${local_portage_dir}
+}
+
+function init_git_repo()
+{
+    if [[ ${#@} -ne 1 ]]; then
+        echo "ERROR: init_git_repo() function takes exactly 1 arguments!"
+        echo "Current arguments: ${@}"
+        return
+    fi
+    cd ${local_portage_dir}
+    if [[ ! -d .git ]]; then
+        git init
+    fi
 }
 
 function sync_and_git_tag()
 {
     # Sync local portage tree
-    #get_latest_portage_snapshot ${mirror} ${portage_snapshot_path} ${portage_snapshot_file}
-    rsync_portage ${PORTAGE_RSYNC_OPTS} ${rsync_mirror}
+    #get_latest_portage_snapshot ${mirror} ${portage_snapshot_path} ${portage_snapshot_file} ${local_portage_dir}
+    rsync_portage ${PORTAGE_RSYNC_OPTS} ${rsync_mirror} ${local_portage_dir}
+
+    init_git_repo ${local_portage_dir}
 
     # Add to git
-    git add ${local_portage_dir}
+    cd ${local_portage_dir}
+    git add *
     git commit -a -m "Automatic portage git commit - ${now}"
     git tag ${now}
+    cd -
 }
 
 # Run it!
